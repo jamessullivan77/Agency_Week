353663db5144103ec61fa6ecf50a0364d09e14c9
Steven <deleon46@optonline.net>
2017-03-08 18:29:25 -0500

    First commit - Steve (homepage)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,45 +15,59 @@
 //= require turbolinks
 //= require_tree .
 
+jQuery(document).ready(function(){
+  $(window).scroll(function(e){
+    parallaxScroll();
+    });
+     
 
-var map;
+ }); 
 
-function initializeMap(location){
+function parallaxScroll(){
+    console.log("parallaxscroll")
+    var scrolled = $(window).scrollTop();
+    $('#parallax-bg-1').css('top',(0-(scrolled*.25))+'px');
+    $('#parallax-bg-2').css('top',(0-(scrolled*.4))+'px');
+    $('#parallax-bg-3').css('top',(0-(scrolled*.75))+'px');
+    $('#parallax-bg-4').css('top',(0-(scrolled*.66))+'px');
+}
+ 
+
+// var map;
+
+// function initializeMap(location){
     	
-    	console.log(location);
+//     	console.log(location);
 
-    	var currentLocation = new google.maps.LatLng(location.coords.latitude, location.coords.longitude);
+//     	var currentLocation = new google.maps.LatLng(location.coords.latitude, location.coords.longitude);
 
-    	var mapsOptions = {
-    		center: currentLocation,
-    		zoom: 20,
-    		mapTypeId: google.maps.MapTypeId.ROADMAP
-    	};
+//     	var mapsOptions = {
+//     		center: currentLocation,
+//     		zoom: 20,
+//     		mapTypeId: google.maps.MapTypeId.ROADMAP
+//     	};
 
-    	 map = new google.maps.Map(document.getElementById('map'), mapsOptions);
+//     	 map = new google.maps.Map(document.getElementById('map'), mapsOptions);
 
-    	 var marker = new google.maps.Marker({
-    	 	position: currentLocation,
-    	 	map: map
+//     	 var marker = new google.maps.Marker({
+//     	 	position: currentLocation,
+//     	 	map: map
     	
-    	});
+//     	});
 			
-				function createInfoWindow(text){
-  				var infowindow = new google.maps.InfoWindow({
-    				content: text
-  				});
-  				return infowindow;
-				}
+// 				function createInfoWindow(text){
+//   				var infowindow = new google.maps.InfoWindow({
+//     				content: text
+//   				});
+//   				return infowindow;
+// 				}
 
-				var info = createInfoWindow(document.getElementById("homeless_marker"));
-						google.maps.event.addListener(marker, 'click', function() {
-  						info.open(map,marker);
-							});
+// 				var info = createInfoWindow(document.getElementById("homeless_marker"));
+// 						google.maps.event.addListener(marker, 'click', function() {
+//   						info.open(map,marker);
+// 							});
 
 
-    	}
-$(document).ready(function(){
-	navigator.geolocation.getCurrentPosition(initializeMap)
-});
+//     	}
 
     
--- db/migrate/20170306213717_create_homelesspeople.rb
+++ db/migrate/20170306213717_create_homelesspeople.rb
@@ -3,11 +3,11 @@ class CreateHomelesspeople < ActiveRecord::Migration[5.0]
     create_table :homelesspeople do |t|
     	t.integer :street_number
     	t.string :steet_name
-    	t.integer :homeless_id
+    	# t.integer :homeless_id
     	t.integer :user_id
-    	t.integer :lat
-    	t.integer :long
-    	t.string :institute_id
+    	t.decimal :lat
+    	t.decimal :long
+    	# t.string :institute_id
 
   	t.timestamps
     end 
