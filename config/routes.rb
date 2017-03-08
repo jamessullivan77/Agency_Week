238c9e1107bef95c9f911a05437d5591a955917e
James Sullivan <james.f.sullivanii@gmail.com>
2017-03-08 18:37:11 -0500

    Google Maps API installed and Customized. Able to add people. Admin visual database started.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,24 @@
 Rails.application.routes.draw do
   devise_for :users
   devise_for :models
+  resources :homelesspeople, defaults: {format: :json}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
   get '/map' => 'homelesspeople#index'
+  
+  post '/map' => 'homelesspeople#create'
+  
   get '/users' => 'users#index'
+  
   get '/users/:id' => 'users#show'
+  
   get '/contact' => 'static#index'
+
+  get '/database' => 'database#index'
+
+  post '/database' => 'homelesspeople#create'
+
+  post '/database' => 'users#create'
+
+  post '/database' => 'users#sign_up'
 end
--- db/migrate/20170306195205_devise_create_users.rb
+++ db/migrate/20170306195205_devise_create_users.rb
@@ -5,6 +5,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      # Custom
+      t.string :fname
+      t.string :lname
+      t.string :username
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
