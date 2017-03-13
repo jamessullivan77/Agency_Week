23a213034222b599043fd002c5e37afb55a1d190
James Sullivan <james.f.sullivanii@gmail.com>
2017-03-13 14:05:43 -0400

    final push

--- db/schema.rb
+++ db/schema.rb
@@ -63,13 +63,13 @@ ActiveRecord::Schema.define(version: 20170311221022) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "fname"
-    t.string   "lname"
-    t.string   "username"
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "fname"
+    t.string   "lname"
+    t.string   "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
